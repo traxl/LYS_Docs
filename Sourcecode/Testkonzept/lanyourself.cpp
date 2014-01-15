fe2260381428f03e77c521fd48ef393d7df99c38
arieppel <arieppel@gmail.com>
2014-01-15 15:28:33 +0100

    Programmablauf geringfügig geändert

--- Sourcecode/Testkonzept/lanyourself.cpp
+++ Sourcecode/Testkonzept/lanyourself.cpp
@@ -31,33 +31,34 @@ LanYourself::~LanYourself()
 void LanYourself::Connect() // defined in .h under public/private slots:
 {
 
-
+    ui->bname->setText("mrxox04");
+    ui->rname->setText("Chillerrunde");
+    ui->pw->setText("password");
     //ui->pw->setText("Irgendwas");
-    std::string line;
-    ifstream myfile ("C:/Users/Alex/Documents/LYS_Docs/Sourcecode/freelan.txt");
-    while ( getline(myfile,line) )
-    {
-        ui->bname->setText("mrxox04");
-        ui->rname->setText("Chillerrunde");
-        // if(line.find(variable,0) != string::npos)
-        //{
-        ui->pw->setText(QString::fromStdString(line));
 
-        //cout << line << "\n";
-       // }
-    }
-    myfile.close();
 }
 void LanYourself::ProNew() // defined in .h under public/private slots:
 {
     ofstream myfile;
     myfile.open ("C:/Users/Alex/Documents/LYS_Docs/Sourcecode/freelan.txt");
-    myfile << "Writing this to a file.\n";
-    ui->ProPath->setText(QString::fromStdString("Success"));
+    myfile << "New Profile";
+    ui->ProPath->setText(QString::fromStdString("New Profile created"));
     myfile.close();
 }
 void LanYourself::ProLoad() // defined in .h under public/private slots:
 {
-    ui->ProPath->setText("Profil Laden");
+    std::string line;
+    ifstream myfile ("C:/Users/Alex/Documents/LYS_Docs/Sourcecode/freelan.txt");
+    while ( getline(myfile,line) )
+    {
+
+        // if(line.find(variable,0) != string::npos)
+        //{
+        ui->ProPath->setText(QString::fromStdString(line));
+
+        //cout << line << "\n";
+       // }
+    }
+    myfile.close();
 }
 
